61efbcce19d7bd3270ed884b043a1273de90ffe2
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 04:40:39 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  create_an_array= [2,3,4,5]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ array= [ ]
+  add_element_to_end_of_array.push("element")
 end
 
 def add_element_to_start_of_array(array, element)
